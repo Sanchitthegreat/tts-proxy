425afce3bd71c90b1bb55c872a10b5e79ae3cbec
Sanchit Chopra <sanchitchopra@Sanchits-MacBook-Pro.local>
2025-08-07 17:39:33 +0530

    fix: backticks and console log

--- server.js
+++ server.js
@@ -8,30 +8,31 @@ const PORT = process.env.PORT || 3000;
 
 // Basic CORS (optional; keep if you had it before)
 app.use((req, res, next) => {
-res.set('Access-Control-Allow-Origin', '*');
-res.set('Access-Control-Allow-Methods', 'GET, OPTIONS');
-res.set('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-if (req.method === 'OPTIONS') return res.sendStatus(204);
-next();
+  res.set('Access-Control-Allow-Origin', '*');
+  res.set('Access-Control-Allow-Methods', 'GET, OPTIONS');
+  res.set('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  if (req.method === 'OPTIONS') return res.sendStatus(204);
+  next();
 });
 
 app.get('/', (req, res) => {
-res.send('Proxy OK');
+  res.send('Proxy OK');
 });
 
 app.get('/proxy', (req, res) => {
-const ts = encodeURIComponent(req.query.ts || Date.now());
-const targetUrl = https://tts.cognitive.microsoft.com/consumer/speech/synthesize/readaloud/edge/v1?trustedclienttoken=1&ts=${ts};
+  const ts = encodeURIComponent(req.query.ts || Date.now());
+  const targetUrl = `https://tts.cognitive.microsoft.com/consumer/speech/synthesize/readaloud/edge/v1?trustedclienttoken=1&ts=${ts}`;
 
-https.get(targetUrl, (upRes) => {
-if (upRes.headers['content-type']) res.set('content-type', upRes.headers['content-type']);
-upRes.pipe(res);
-}).on('error', (err) => {
-res.status(502).send('Proxy error: ' + err.message);
-});
+  https.get(targetUrl, (upRes) => {
+    if (upRes.headers['content-type']) {
+      res.set('content-type', upRes.headers['content-type']);
+    }
+    upRes.pipe(res);
+  }).on('error', (err) => {
+    res.status(502).send('Proxy error: ' + err.message);
+  });
 });
 
 app.listen(PORT, () => {
-console.log(Proxy on ${PORT});
+  console.log(`Proxy on ${PORT}`);
 });
-
